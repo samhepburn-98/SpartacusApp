81cc2817f5c02c96c79f7f73454597a8314ffe34
samhepburn-98 <samhepburn98@msn.com>
2022-03-21 19:18:32 +0000

    Update app

--- App.tsx
+++ App.tsx
@@ -1,51 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from "react";
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, } from "react-native";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, useColorScheme } from "react-native";
 
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-
-const Section: React.FC<{
-    title: string;
-}> = ({ children, title }) => {
-    const isDarkMode = useColorScheme() === "dark";
-    return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}>
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}>
-                {children}
-            </Text>
-        </View>
-    );
-};
+import { Colors, Header } from "react-native/Libraries/NewAppScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
     const isDarkMode = useColorScheme() === "dark";
@@ -55,33 +12,17 @@ const App = () => {
     };
 
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"}/>
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}>
-                <Header/>
-                <View
-                    style={{
-                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                    }}>
-                    <Section title="Step One">
-                        Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                        screen and then come back to see your edits.
-                    </Section>
-                    <Section title="See Your Changes">
-                        <ReloadInstructions/>
-                    </Section>
-                    <Section title="Debug">
-                        <DebugInstructions/>
-                    </Section>
-                    <Section title="Learn More">
-                        Read the docs to discover what to do next:
-                    </Section>
-                    <LearnMoreLinks/>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+        <NavigationContainer>
+            <SafeAreaView style={backgroundStyle}>
+                <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"}/>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={backgroundStyle}>
+                    <Header/>
+                    Hello world
+                </ScrollView>
+            </SafeAreaView>
+        </NavigationContainer>
     );
 };
 
