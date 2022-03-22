03c6cd01d1c9982edb2adf2457b94079628a9229
samhepburn-98 <samhepburn98@msn.com>
2022-03-22 16:58:58 +0000

    Populate home screen

--- containers/HomeScreen.tsx
+++ containers/HomeScreen.tsx
@@ -1,1 +1,26 @@
-export {};
+import React from "react";
+import { Button, Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/Navigation";
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "Home">;
+
+const HomeScreen = ({ navigation }: HomeScreenProps) => {
+    return (
+        <View>
+            <Text>
+                Home
+            </Text>
+            <Button
+                title="Go to Create Room"
+                onPress={() => navigation.navigate("CreateRoom")}
+            />
+            <Button
+                title="Go to Join Room"
+                onPress={() => navigation.navigate("JoinRoom")}
+            />
+        </View>
+    );
+};
+
+export default HomeScreen;
