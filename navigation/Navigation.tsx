0ca159c693a9d00382d3bda5f483084b0cdb1e19
samhepburn-98 <samhepburn98@msn.com>
2022-03-23 19:11:44 +0000

    Add lobby to stack nav

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -4,22 +4,25 @@ import CreateRoomScreen from "../containers/CreateRoomScreen";
 import JoinRoomScreen from "../containers/JoinRoomScreen";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LobbyScreen from "../containers/LobbyScreen";
 
 export type RootStackParamList = {
     Home: undefined;
     CreateRoom: undefined;
     JoinRoom: undefined;
+    Lobby: undefined;
 };
 
 const Navigation = () => {
     const Stack = createNativeStackNavigator<RootStackParamList>();
 
-    return(
+    return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen}/>
                 <Stack.Screen name="CreateRoom" component={CreateRoomScreen}/>
                 <Stack.Screen name="JoinRoom" component={JoinRoomScreen}/>
+                <Stack.Screen name="Lobby" component={LobbyScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
