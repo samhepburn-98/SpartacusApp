8996072d0e86218b18d0a68135c0e78faba21f44
samhepburn-98 <samhepburn98@msn.com>
2022-03-23 10:57:44 +0000

    Add join room logic

--- containers/JoinRoomScreen.tsx
+++ containers/JoinRoomScreen.tsx
@@ -1,14 +1,58 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useCallback } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import useSocket from "../hooks/useSocket";
+
+type FormData = {
+    roomCode: string;
+};
 
 const JoinRoomScreen = () => {
+    const socket = useSocket();
+
+    const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const handleJoinRoom = useCallback((data) => {
+        socket.emit("joinRoom", data.roomCode);
+        console.log(data);
+    }, []);
+
     return (
         <View>
             <Text>
                 Join Room
             </Text>
+            <Controller
+                control={control}
+                name="roomCode"
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                rules={{
+                    required: true,
+                }}
+            />
+            {errors.roomCode && <Text>This is required.</Text>}
+            <Button
+                onPress={handleSubmit(handleJoinRoom)}
+                title="Enter"
+            />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
 
 export default JoinRoomScreen;
