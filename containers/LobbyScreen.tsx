920483869f75e027bd94c98d3b3ec82c2d036157
samhepburn-98 <samhepburn98@msn.com>
2022-03-23 19:10:38 +0000

    Add lobby screen

---
+++ containers/LobbyScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LobbyScreen = () => {
+    return (
+        <View>
+            <Text>
+                Lobby
+            </Text>
+        </View>);
+};
+
+export default LobbyScreen;
