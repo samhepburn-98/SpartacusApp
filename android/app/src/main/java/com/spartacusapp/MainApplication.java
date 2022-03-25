6bba00a79f5530a01bba4c6ce210657d7ecccc20
samhepburn-98 <samhepburn98@msn.com>
2022-03-25 17:22:45 +0000

    Update android dev config

--- android/app/src/main/java/com/spartacusapp/MainApplication.java
+++ android/app/src/main/java/com/spartacusapp/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.reactnativecommunity.cameraroll.CameraRollPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -18,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
       new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
+          return true;
         }
 
         @Override
@@ -27,8 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-
-          packages.add(new CameraRollPackage());
           return packages;
         }
 
