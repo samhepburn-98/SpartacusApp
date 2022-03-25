7c416459bcc2c37aa071b742372b2ddbf0467df0
samhepburn-98 <samhepburn98@msn.com>
2022-03-25 11:49:11 +0000

    Update local server url for android socket compatibility

--- providers/SocketProvider.tsx
+++ providers/SocketProvider.tsx
@@ -1,7 +1,8 @@
 import React, { createContext, ReactChild, useContext } from "react";
 import { io } from "socket.io-client";
+import { Platform } from "react-native";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = Platform.OS === "android" ? "http://10.0.2.2:3000" : "http://127.0.0.1:3000";
 export const socket = io(SOCKET_URL);
 export const SocketContext = createContext(socket);
 
