80bf863b8c687cfaa1976f623bdbab8c55c11c08
samhepburn-98 <samhepburn98@msn.com>
2022-03-27 14:36:27 +0100

    Hello world results screen

--- containers/ResultsScreen.tsx
+++ containers/ResultsScreen.tsx
@@ -1,1 +1,14 @@
-export {};
+import { Text } from "native-base";
+import { Dispatch, SetStateAction } from "react";
+
+type ResultsScreenProps = {
+    onContinue: Dispatch<SetStateAction<"selection" | "results">>;
+}
+
+const ResultsScreen = ({ onContinue }: ResultsScreenProps) => {
+    return (
+        <Text>Helloworld</Text>
+    );
+};
+
+export default ResultsScreen;
