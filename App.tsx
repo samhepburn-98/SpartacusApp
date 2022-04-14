5a9c53dd218da7c250af9bad37f64658bf83ca45
samhepburn-98 <samhepburn98@msn.com>
2022-04-14 23:50:37 +0100

    Remove unused styles in app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { NativeBaseProvider } from "native-base";
 import React from "react";
-import { StyleSheet, useColorScheme } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 import Navigation from "./navigation/Navigation";
 import { SocketProvider } from "./providers/SocketProvider";
 import { theme } from "./theme/theme";
@@ -11,11 +9,6 @@ import { ColorsProvider } from "./providers/ColorsProvider";
 
 
 const App = () => {
-    const isDarkMode = useColorScheme() === "dark";
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
     const randomColors: string[] = randomPropertyInObj(GRADIENT_PAIRS) as string[];
 
     return (
@@ -28,24 +21,5 @@ const App = () => {
         </NativeBaseProvider>
     );
 };
-
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: "600",
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: "400",
-    },
-    highlight: {
-        fontWeight: "700",
-    },
-});
 
 export default App;
