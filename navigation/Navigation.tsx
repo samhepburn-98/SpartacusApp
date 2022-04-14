8918f6abe83f02eea4e0963676db3f6b36d17aa2
samhepburn-98 <samhepburn98@msn.com>
2022-04-14 23:52:23 +0100

    Add header options to navigation

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../containers/HomeScreen";
 import CreateRoomScreen from "../containers/CreateRoomScreen";
 import JoinRoomScreen from "../containers/JoinRoomScreen";
-import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LobbyScreen from "../containers/LobbyScreen";
 import GameScreen from "../containers/GameScreen";
+import Header from "../components/Header";
 
 export type RootStackParamList = {
     CreateRoom: undefined;
@@ -21,12 +22,33 @@ const Navigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="CreateRoom" component={CreateRoomScreen}/>
-                <Stack.Screen name="JoinRoom" component={JoinRoomScreen}/>
-                <Stack.Screen name="Lobby" component={LobbyScreen}/>
-                <Stack.Screen name="Game" component={GameScreen}/>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ header: () => <Header/>, }}
+                />
+                <Stack.Screen
+                    name="CreateRoom"
+                    component={CreateRoomScreen}
+                    options={{ header: () => <Header showBack/>, }}
+                />
+                <Stack.Screen
+                    name="JoinRoom"
+                    component={JoinRoomScreen}
+                    options={{ header: () => <Header showBack/>, }}
+                />
+                <Stack.Screen
+                    name="Lobby"
+                    component={LobbyScreen}
+                    options={{ header: () => <Header showBack/>, }}
+                />
+                <Stack.Screen
+                    name="Game"
+                    component={GameScreen}
+                    options={{ header: () => <Header showBack/>, }}
+                />
             </Stack.Navigator>
+
         </NavigationContainer>
     );
 };
